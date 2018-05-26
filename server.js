de980b1ef1737a9153ef4aee8014a4258bda5e71
Shreyansh jain <shreyanshjain.2015@vit.ac.in>
2018-05-26 14:27:29 +0530

    Add GET todos id

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser'); 
+var {ObjectID} = require('mongodb');
 
 var {mongoose} =    require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -23,6 +24,23 @@ app.post('/todos',(req,res)=>{
 app.get('/todos', (req,res)=>{
     Todo.find().then((todos)=>{
         res.send({todos});
+    },(e)=>{
+        res.status(400).send(e);
+    });
+});
+
+
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.send(404).send(); 
+    }
+
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.send(404).send();
+        }
+        res.send({todo});
     },(e)=>{
         res.status(400).send(e);
     });
